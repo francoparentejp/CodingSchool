94d9993aac9a87d720840518405676dbc9a42988
francoparentejp <116745470+francoparentejp@users.noreply.github.com>
2022-11-03 10:35:58 -0300

    Create Punto.py

---
+++ Python/Objetos Y Clases/src/Punto.py
@@ -0,0 +1,12 @@
+class Point:
+
+    def __init__(self, an_x, an_y):
+        self._y = an_y
+        self._x = an_x
+
+    def x(self):
+        return self._x
+
+    def y(self):
+        return self._y
+
